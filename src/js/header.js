540f2c687ea926c019ddb1657a596480646abbfb
Sergio Coria <sergiocoria.my@gmail.com>
2025-06-07 19:14:57 -0600

    4.2

--- src/js/header.js
+++ src/js/header.js
@@ -4,10 +4,10 @@ export function loadHeader() {
       <nav>
         <h1>Mindful Therapy</h1>
             <ul>
-            <li><a href="/index.html">Home</a></li>
-            <li><a href="/services.html">Services</a></li>
-            <li><a href="/book.html">Book</a></li>
-            <li><a href="/testimonies.html">Testimonials</a></li>
+            <li><a href="index.html">Home</a></li>
+            <li><a href="services.html">Services</a></li>
+            <li><a href="book.html">Book</a></li>
+            <li><a href="testimonies.html">Testimonials</a></li>
             </ul>
       </nav>
     </header>

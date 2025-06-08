abe604d31fa575fd2bd40590c116e433065620a3
Sergio Coria <sergiocoria.my@gmail.com>
2025-06-07 18:53:20 -0600

    3.2

--- src/js/footer.js
+++ src/js/footer.js
@@ -4,13 +4,13 @@ export function loadFooter() {
       <p>&copy; 2025 Mindful Therapy. All rights reserved.</p>
       <div class="social-links">
         <a href="https://www.facebook.com" target="_blank" rel="noopener">
-          <img src="/public/images/facebook.svg" alt="Facebook" class="social-icon">
+          <img src="./public/images/facebook.svg" alt="Facebook" class="social-icon">
         </a>
         <a href="https://www.instagram.com" target="_blank" rel="noopener">
-          <img src="/public/images/instagram.svg" alt="Instagram" class="social-icon">
+          <img src="./public/images/instagram.svg" alt="Instagram" class="social-icon">
         </a>
         <a href="https://www.twitter.com" target="_blank" rel="noopener">
-          <img src="/public/images/x.svg" alt="X" class="social-icon">
+          <img src="./public/images/x.svg" alt="X" class="social-icon">
         </a>
       </div>
     </footer>
